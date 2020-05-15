b15013e2d131320dc6cadc0ee18e35bace4f060d
crazyq <crazyq@gmail.com>
2020-05-15 10:27:48 +0800

    简体中文汉化翻译设置页面

--- libretro/libretro_core_options_intl.h
+++ libretro/libretro_core_options_intl.h
@@ -372,6 +372,522 @@ struct retro_core_option_definition option_defs_pt_br[] = {
 
 /* RETRO_LANGUAGE_CHINESE_SIMPLIFIED */
 
+struct retro_core_option_definition option_defs_chs[] = {
+   {
+      "genesis_plus_gx_system_hw",
+      "系统硬件",
+      "以指定的模拟硬件运行载入的游戏，'自动'会根据当前游戏选择最合适的系统。",
+      {
+         { "auto",                 "自动"               },
+         { "sg-1000",              "SG-1000"            },
+         { "sg-1000 II",           "SG-1000 II"         },
+         { "mark-III",             "Mark III"           },
+         { "master system",        "Master System"      },
+         { "master system II",     "Master System II"   },
+         { "game gear",            "Game Gear"          },
+         { "mega drive / genesis", "Mega Drive/Genesis" },
+         { NULL, NULL },
+      },
+      "auto"
+   },
+   {
+      "genesis_plus_gx_region_detect",
+      "系统制式",
+      "指定系统区域制式。除Game Gear外，'PAL'是50hz而'NTSC'是60hz。\n"
+      "如果选择了不正确的制式，游戏可能比正常运行速度偏快或者偏慢。",
+      {
+         { "auto",    "自动"   },
+         { "ntsc-u",  "NTSC-U" },
+         { "pal",     "PAL"    },
+         { "ntsc-j",  "NTSC-J" },
+         { NULL, NULL },
+      },
+      "auto"
+   },
+   {
+      "genesis_plus_gx_force_dtack",
+      "系统锁定",
+      "模拟真实硬件上执行非法地址访问时发生的系统锁定。\n"
+      "此设置应该只在运行特定演示或者自制游戏时关闭，这些游戏可能依赖非法行为来完成操作。",
+      {
+         { "enabled",  "启用" },
+         { "disabled", "禁用" },
+         { NULL, NULL },
+      },
+      "enabled"
+   },
+   {
+      "genesis_plus_gx_bios",
+      "系统启动ROM",
+      "使用官方BIOS/启动器加载器，如果在RetroArch系统目录中存在的话。\n"
+      "显示机种特定启动动画，然后运行载入的游戏。",
+      {
+         { "disabled", "禁用" },
+         { "enabled",  "启用" },
+         { NULL, NULL },
+      },
+      "disabled"
+   },
+   {
+      "genesis_plus_gx_bram",
+      "CD系统BRAM",
+      "运行Sega CD游戏时，指定是在每个区域的游戏（按区域）中共享存档文件，还是为每个游戏（按游戏）单独保存存档。 \n"
+      "注意：Sega CD内部存储容量有限，只够保存几个游戏。为了避免存储空间不够用，建议选择‘按游戏’。",
+      {
+         { "per bios", "按区域" },
+         { "per game", "按游戏" },
+         { NULL, NULL },
+      },
+      "per bios"
+   },
+   {
+      "genesis_plus_gx_addr_error",
+      "68K寻址错误",
+      "MD的CPU（摩托罗拉68000）在执行未对齐内存访问时会产生一个寻址错误（宕机）。\n"
+      "启用此设置模拟该行为。此设置应该只在运行修改版ROM时关闭，\n"
+      "因为这些ROM通常是在不精确的模拟器上修改的，可能依赖于无效的内存访问才能正确运行。",
+      {
+         { "enabled",  "启用" },
+         { "disabled", "禁用" },
+         { NULL, NULL },
+      },
+      "enabled"
+   },
+   {
+      "genesis_plus_gx_lock_on",
+      "卡带锁定",
+      "锁定技术是MD的一项功能，该功能允许老游戏连接到特殊卡带的传输端口上，进行扩展游戏或者实现附加功能。\n"
+      "此设置指定模拟何种类型的‘锁定’功能。RetroArch系统目录中必须有相对应的BIOS文件。",
+      {
+         { "disabled",            "禁用" },
+         { "game genie",          "Game Genie" },
+         { "action replay (pro)", "Action Replay (Pro)" },
+         { "sonic & knuckles",    "Sonic & Knuckles" },
+         { NULL, NULL },
+      },
+      "disabled"
+   },
+   {
+      "genesis_plus_gx_ym2413",
+      "Master System FM音源（YM2413）",
+      "启用Sega Mark III/Master System FM声音单元的模拟，以增强某些游戏的声音输出。",
+      {
+         { "auto",     "自动" },
+         { "disabled", "禁用" },
+         { "enabled",  "启用" },
+         { NULL, NULL },
+      },
+      "auto"
+   },
+   {
+      "genesis_plus_gx_ym2612",
+      "Mega Drive / Genesis FM音源",
+#ifdef HAVE_YM3438_CORE
+      "选择模拟MD/Genesis FM合成器（主声音发生器）的方式。\n"
+      "'MAME'方式较快，在绝大多数系统上都可以全速运行。\n"
+      "'Nuked'方式较精确，声音质量更高，但是需要更高的CPU性能。\n"
+      "'YM2612'芯片由早期MD1型机使用，'YM3438'由后期MD机型使用。",
+#else
+      "选择模拟MD/Genesis FM合成器（主声音发生器）的方式。\n"
+      "'YM2612'芯片由早期MD1型机使用，'YM3438'由后期MD机型使用。",
+#endif
+      {
+         { "mame (ym2612)",          "MAME (YM2612)" },
+         { "mame (asic ym3438)",     "MAME (ASIC YM3438)" },
+         { "mame (enhanced ym3438)", "MAME (Enhanced YM3438)" },
+#ifdef HAVE_YM3438_CORE
+         { "nuked (ym2612)",         "Nuked (YM2612)" },
+         { "nuked (ym3438)",         "Nuked (YM3438)" },
+#endif
+         { NULL, NULL },
+      },
+      "mame (ym2612)"
+   },
+   {
+      "genesis_plus_gx_sound_output",
+      "输出声道",
+      "选择立体声或单声道声音输出。",
+      {
+         { "stereo", "立体声" },
+         { "mono",   "单声道" },
+         { NULL, NULL },
+      },
+      "stereo"
+   },
+   {
+      "genesis_plus_gx_psg_preamp",
+      "PSG预放大级别",
+      "设置的SN76496四通道可编程声音发生器的声音预放大级别，\n"
+      "此芯片用于Master System、Game Gear和MD/Genesis。",
+      {
+         { "0",   NULL },
+         { "5",   NULL },
+         { "10",  NULL },
+         { "15",  NULL },
+         { "20",  NULL },
+         { "25",  NULL },
+         { "30",  NULL },
+         { "35",  NULL },
+         { "40",  NULL },
+         { "45",  NULL },
+         { "50",  NULL },
+         { "55",  NULL },
+         { "60",  NULL },
+         { "65",  NULL },
+         { "70",  NULL },
+         { "75",  NULL },
+         { "80",  NULL },
+         { "85",  NULL },
+         { "90",  NULL },
+         { "95",  NULL },
+         { "100", NULL },
+         { "105", NULL },
+         { "110", NULL },
+         { "115", NULL },
+         { "120", NULL },
+         { "125", NULL },
+         { "130", NULL },
+         { "135", NULL },
+         { "140", NULL },
+         { "145", NULL },
+         { "150", NULL },
+         { "155", NULL },
+         { "160", NULL },
+         { "165", NULL },
+         { "170", NULL },
+         { "175", NULL },
+         { "180", NULL },
+         { "185", NULL },
+         { "190", NULL },
+         { "195", NULL },
+         { "200", NULL },
+         { NULL, NULL },
+      },
+      "150"
+   },
+   {
+      "genesis_plus_gx_fm_preamp",
+      "FM预放大级别",
+      "设置FM音频芯片的预放大级别，此芯片用于Sega Mark III/Master System。",
+      {
+         { "0",   NULL },
+         { "5",   NULL },
+         { "10",  NULL },
+         { "15",  NULL },
+         { "20",  NULL },
+         { "25",  NULL },
+         { "30",  NULL },
+         { "35",  NULL },
+         { "40",  NULL },
+         { "45",  NULL },
+         { "50",  NULL },
+         { "55",  NULL },
+         { "60",  NULL },
+         { "65",  NULL },
+         { "70",  NULL },
+         { "75",  NULL },
+         { "80",  NULL },
+         { "85",  NULL },
+         { "90",  NULL },
+         { "95",  NULL },
+         { "100", NULL },
+         { "105", NULL },
+         { "110", NULL },
+         { "115", NULL },
+         { "120", NULL },
+         { "125", NULL },
+         { "130", NULL },
+         { "135", NULL },
+         { "140", NULL },
+         { "145", NULL },
+         { "150", NULL },
+         { "155", NULL },
+         { "160", NULL },
+         { "165", NULL },
+         { "170", NULL },
+         { "175", NULL },
+         { "180", NULL },
+         { "185", NULL },
+         { "190", NULL },
+         { "195", NULL },
+         { "200", NULL },
+         { NULL, NULL },
+      },
+      "100"
+   },
+   {
+      "genesis_plus_gx_audio_filter",
+      "音频过滤",
+      "启用音频低通滤镜，以更好地模拟MD1型机的声音特性。",
+      {
+         { "disabled", "禁用" },
+         { "low-pass", "Low-Pass" },
+         { NULL, NULL },
+      },
+      "disabled"
+   },
+   {
+      "genesis_plus_gx_lowpass_range",
+      "低通滤镜强度%",
+      "设置低通滤镜的截断频率。\n"
+      "值越高，可察觉到的过滤强度越高，因为更宽范围的高频扩展被减弱了。",
+      {
+         { "5",  NULL },
+         { "10", NULL },
+         { "15", NULL },
+         { "20", NULL },
+         { "25", NULL },
+         { "30", NULL },
+         { "35", NULL },
+         { "40", NULL },
+         { "45", NULL },
+         { "50", NULL },
+         { "55", NULL },
+         { "60", NULL },
+         { "65", NULL },
+         { "70", NULL },
+         { "75", NULL },
+         { "80", NULL },
+         { "85", NULL },
+         { "90", NULL },
+         { "95", NULL },
+         { NULL, NULL },
+      },
+      "60"
+   },
+#ifdef HAVE_EQ
+   {
+      "genesis_plus_gx_audio_eq_low",
+      "EQ低频",
+      "调整内置音频均衡器的低音频带。",
+      {
+         { "0",   NULL },
+         { "5",   NULL },
+         { "10",  NULL },
+         { "15",  NULL },
+         { "20",  NULL },
+         { "25",  NULL },
+         { "30",  NULL },
+         { "35",  NULL },
+         { "40",  NULL },
+         { "45",  NULL },
+         { "50",  NULL },
+         { "55",  NULL },
+         { "60",  NULL },
+         { "65",  NULL },
+         { "70",  NULL },
+         { "75",  NULL },
+         { "80",  NULL },
+         { "85",  NULL },
+         { "90",  NULL },
+         { "95",  NULL },
+         { "100", NULL },
+         { NULL, NULL },
+      },
+      "100"
+   },
+   {
+      "genesis_plus_gx_audio_eq_mid",
+      "EQ中频",
+      "调整内置音频均衡器的中音频带。",
+      {
+         { "0",   NULL },
+         { "5",   NULL },
+         { "10",  NULL },
+         { "15",  NULL },
+         { "20",  NULL },
+         { "25",  NULL },
+         { "30",  NULL },
+         { "35",  NULL },
+         { "40",  NULL },
+         { "45",  NULL },
+         { "50",  NULL },
+         { "55",  NULL },
+         { "60",  NULL },
+         { "65",  NULL },
+         { "70",  NULL },
+         { "75",  NULL },
+         { "80",  NULL },
+         { "85",  NULL },
+         { "90",  NULL },
+         { "95",  NULL },
+         { "100", NULL },
+         { NULL, NULL },
+      },
+      "100"
+   },
+   {
+      "genesis_plus_gx_audio_eq_high",
+      "EQ高频",
+      "调整内置音频均衡器的高音频带。",
+      {
+         { "0",   NULL },
+         { "5",   NULL },
+         { "10",  NULL },
+         { "15",  NULL },
+         { "20",  NULL },
+         { "25",  NULL },
+         { "30",  NULL },
+         { "35",  NULL },
+         { "40",  NULL },
+         { "45",  NULL },
+         { "50",  NULL },
+         { "55",  NULL },
+         { "60",  NULL },
+         { "65",  NULL },
+         { "70",  NULL },
+         { "75",  NULL },
+         { "80",  NULL },
+         { "85",  NULL },
+         { "90",  NULL },
+         { "95",  NULL },
+         { "100", NULL },
+         { NULL, NULL },
+      },
+      "100"
+   },
+#endif
+   {
+      "genesis_plus_gx_blargg_ntsc_filter",
+      "Blargg NTSC滤镜",
+      "应用一个视频滤镜来模拟不同的NTSC电视信号。",
+      {
+         { "disabled",   "禁用" },
+         { "monochrome", "单色" },
+         { "composite",  "复合视频" },
+         { "svideo",     "S-Video" },
+         { "rgb",        "RGB" },
+         { NULL, NULL },
+      },
+      "disabled"
+   },
+   {
+      "genesis_plus_gx_lcd_filter",
+      "LCD鬼影滤镜",
+      "应用一个图像鬼影滤镜，来模拟Game Gear和Genesis Nomad的LCD屏幕显示特性。",
+      {
+         { "disabled", "禁用" },
+         { "enabled",  "启用" },
+         { NULL, NULL },
+      },
+      "disabled"
+   },
+   {
+      "genesis_plus_gx_overscan",
+      "边界",
+      "启用此设置显示屏幕上下或者左右的过扫描区域。\n"
+      "这些区域通常被标准电视的边框隐藏了。",
+      {
+         { "disabled",   "禁用" },
+         { "top/bottom", "上/下" },
+         { "left/right", "左/右" },
+         { "full",       "全部" },
+         { NULL, NULL },
+      },
+      "disabled"
+   },
+   {
+      "genesis_plus_gx_gg_extra",
+      "扩展Game Gear屏幕",
+      "强制Game Gear游戏以SMS模式运行，提高分辨率到256x192。\n"
+      "可能会显示额外的内容，但是通常是显示一个含有无效图像数据的边框。",
+      {
+         { "disabled", "禁用" },
+         { "enabled",  "启用" },
+         { NULL, NULL },
+      },
+      "disabled"
+   },
+   {
+      "genesis_plus_gx_aspect_ratio",
+      "内核提供宽高比",
+      "选择期望的显示宽高比。\n"
+      "此项设置只有当RetroArch的宽高比设为‘内核提供’时有效。",
+      {
+         { "auto",     "自动" },
+         { "NTSC PAR", "NTSC像素宽高比" },
+         { "PAL PAR",  "PAL像素宽高比" },
+      },
+      "auto"
+   },
+   {
+      "genesis_plus_gx_render",
+      "隔行模式2输出",
+      "隔行模式2允许MD/Genesis在每帧绘制额外的扫描线，输出双倍高度的图像（320x448）\n"
+      "此功能由'Sonic the Hedgehog 2'和'Combat Cars'多人游戏模式使用。\n"
+      "'双场'模拟原始硬件，输出更锐利的图像同时伴随抖动/隔行扫描效果。\n"
+      "'单场'应用一个去隔行滤镜，产生稳定的图像但是可能导致轻微模糊。",
+      {
+         { "single field", "Single Field" },
+         { "double field", "Double Field" },
+         { NULL, NULL },
+      },
+      "single field"
+   },
+   {
+      "genesis_plus_gx_gun_cursor",
+      "显示光枪准心",
+      "在使用'MD Menacer', 'MD Justifiers'和'MS Light Phaser'输入设备时显示光枪准心。",
+      {
+         { "disabled", "禁用" },
+         { "enabled",  "启用" },
+         { NULL, NULL },
+      },
+      "disabled"
+   },
+   {
+      "genesis_plus_gx_gun_input",
+      "光枪输入",
+      "使用鼠标控制的‘光枪’或者‘触屏’输入。",
+      {
+         { "lightgun",    "光枪" },
+         { "touchscreen", "触屏" },
+         { NULL, NULL },
+      },
+      "lightgun"
+   },
+   {
+      "genesis_plus_gx_invert_mouse",
+      "鼠标Y轴反向",
+      "使用'MD鼠标'输入设备时反向Y轴。",
+      {
+         { "disabled", "禁用" },
+         { "enabled",  "启用" },
+         { NULL, NULL },
+      },
+      "disabled"
+   },
+#ifdef HAVE_OVERCLOCK
+   {
+      "genesis_plus_gx_overclock",
+      "CPU速度",
+      "对模拟CPU进行超频。可以减少拖慢，但可能导致其他问题。",
+      {
+         { "100%", NULL },
+         { "125%", NULL },
+         { "150%", NULL },
+         { "175%", NULL },
+         { "200%", NULL },
+         { NULL, NULL },
+      },
+      "100%"
+   },
+#endif
+   {
+      "genesis_plus_gx_no_sprite_limit",
+      "移除每扫描线角色限制",
+      "移除每扫描线的8个（Master System）或20个（MD/Genesis）角色数量硬件限制。\n"
+      "可以减少闪烁，但是可能导致视觉效果不正常，因为有些游戏使用此限制来产生特殊效果。",
+      {
+         { "disabled", "禁用" },
+         { "enabled",  "启用" },
+         { NULL, NULL },
+      },
+      "disabled"
+   },
+   { NULL, NULL, NULL, {{0}}, NULL },
+};
+
 /* RETRO_LANGUAGE_ESPERANTO */
 
 /* RETRO_LANGUAGE_POLISH */
